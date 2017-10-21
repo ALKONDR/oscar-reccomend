c2185c1a193147ae7234059b5e3b705cba2450bb
Alexander Kondrashkin <alkondr@yandex-team.ru>
2017-10-21 06:38:03 +0300

    add transaction end point

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('id: ' + req.query.id);
+});
+
+app.post('/transaction', function(req, res) {
+    console.log(req.query.id);
+    console.log(req.body);
 });
 
 app.listen(8080, function () {
