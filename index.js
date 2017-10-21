73624c3cfa3705638d7f9cf14badfe9ac72db747
Alexander Kondrashkin <alkondr@yandex-team.ru>
2017-10-21 07:31:11 +0300

    fix transaction endpoint

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+var bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('id: ' + req.query.id);
@@ -8,6 +12,7 @@ app.get('/', function(req, res) {
 app.post('/transaction', function(req, res) {
     console.log(req.query.id);
     console.log(req.body);
+    res.sendStatus(200);
 });
 
 app.listen(8080, function () {
